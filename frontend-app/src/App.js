1604211ed63b95926670a6b9f2c26eae103d613c
fulprimitiv <eremeevdima51@gmail.com>
2024-12-05 02:04:26 +0500

    Сверстал начальный экран, сразу добавил адаптив. Сбросил все стили в css, подключил к index.js. Обновил title + favicon.ico. Добавил роутинг. Добавил пару необходимых библиотек.

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomeScreen from './components/WelcomeScreen/WelcomeScreen';
 import EventList from './components/EventList';
 import EventCreate from './components/EventCreate';
 import EventRegister from './components/EventRegister';
 import EventDetails from './components/EventDetails';
-import Form from './components/Form';
 
 function App() {
   return (
-    <div>
-      <h1>Мероприятия</h1>
-      <EventList />
-      <EventCreate />
-      <EventRegister />
-      <EventDetails />
-      <Form />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          {/* Начальный экран */}
+          <Route path="/" element={<WelcomeScreen />} />
+          
+          {/* Экран создания новой анкеты */}
+          <Route path="/create" element={<EventCreate />} />
+          
+          {/* Экран с существующей анкетой по коду */}
+          <Route path="/form/:formId" element={<EventDetails />} />
+          
+          {/* Остальные маршруты */}
+          <Route path="/events" element={<EventList />} />
+          <Route path="/register" element={<EventRegister />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
